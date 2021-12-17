7ec28a354150f8a30b6d25e66d15e65b3d15103d
hikobe8 <hikobe8@163.com>
2021-12-17 17:52:21 +0800

    fix bug & release version 0.3

--- router-runtime/src/main/java/com/ray/router_runtime/RayRouter.kt
+++ router-runtime/src/main/java/com/ray/router_runtime/RayRouter.kt
@@ -6,7 +6,80 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
+import android.text.TextUtils
 import android.util.Log
+
+class Route {
+
+    private val startIntent = Intent()
+    var targetActivityClass: String = ""
+    var requestCode = -1
+    var flags = 0
+
+    fun requestCode(code: Int): Route {
+        requestCode = code
+        return this
+    }
+
+    fun withFlags(flag: Int): Route {
+        flags = flag
+        return this
+    }
+
+    fun putExtra(key: String, value: Any): Route {
+        when (value) {
+            is Boolean -> {
+                startIntent.putExtra(key, value)
+            }
+            is Int -> {
+                startIntent.putExtra(key, value)
+            }
+            is Long -> {
+                startIntent.putExtra(key, value)
+            }
+            is Float -> {
+                startIntent.putExtra(key, value)
+            }
+            is Double -> {
+                startIntent.putExtra(key, value)
+            }
+            is String -> {
+                startIntent.putExtra(key, value)
+            }
+            is Bundle -> {
+                startIntent.putExtra(key, value)
+            }
+            is Parcelable -> {
+                startIntent.putExtra(key, value)
+            }
+        }
+        return this
+    }
+
+    fun navigation(context: Context) {
+        try {
+            val targetActivity = Class.forName(targetActivityClass)
+            startIntent.setClass(context, targetActivity)
+            if (flags > 0)
+                startIntent.flags = flags
+            if (requestCode > -1) {
+                if (context is Activity) {
+                    context.startActivityForResult(startIntent, requestCode)
+                } else {
+                    Log.e(
+                        "RayRouter",
+                        "$context can not call startActivityForResult() method with requestCode $requestCode"
+                    )
+                }
+            } else {
+                context.startActivity(startIntent)
+            }
+        } catch (e: Throwable) {
+            Log.e("RayRouter", "error while start Activity = $targetActivityClass, e = $e")
+        }
+    }
+
+}
 
 object RayRouter {
 
@@ -14,11 +87,6 @@ object RayRouter {
     private const val CLASS_NAME = "com.ray.router.mapping.generated.RouterMapping"
 
     private val mapping = HashMap<String, String>()
-
-    private var targetActivityClass = ""
-    private var requestCode = -1
-    private var startIntent: Intent? = null
-    private var flags = 0
 
     fun init() {
         try {
@@ -37,8 +105,7 @@ object RayRouter {
         }
     }
 
-    fun build(url: String): RayRouter {
-        startIntent = Intent()
+    fun build(url: String): Route {
         val destUri = Uri.parse(url)
         val destScheme = destUri.scheme
         val destHost = destUri.host
@@ -46,90 +113,26 @@ object RayRouter {
         if (mapping.size < 1) {
             Log.e(TAG, "RouterMapping is empty!!! did you call RayRouter.init() method?")
         }
+        val route = Route()
         mapping.onEach {
             val uri = Uri.parse(it.key)
             val scheme = uri.scheme
             val host = uri.host
             val path = uri.path
             if (destScheme == scheme && destHost == host && destPath == path) {
-                targetActivityClass = it.value
+                route.targetActivityClass = it.value
                 return@onEach
             }
         }
-        if (targetActivityClass.isEmpty()) {
+        if (TextUtils.isEmpty(route.targetActivityClass)) {
             Log.e(TAG, "error url = $url, no target Activity found!")
         }
-        return this
-    }
-
-    fun requestCode(code: Int): RayRouter {
-        requestCode = code
-        return this
-    }
-
-    fun withFlags(flag: Int): RayRouter {
-        flags = flag
-        return this
-    }
-
-    fun putExtra(key: String, value: Any): RayRouter {
-        when (value) {
-            is Boolean -> {
-                startIntent?.putExtra(key, value)
-            }
-            is Int -> {
-                startIntent?.putExtra(key, value)
-            }
-            is Long -> {
-                startIntent?.putExtra(key, value)
-            }
-            is Float -> {
-                startIntent?.putExtra(key, value)
-            }
-            is Double -> {
-                startIntent?.putExtra(key, value)
-            }
-            is String -> {
-                startIntent?.putExtra(key, value)
-            }
-            is Bundle -> {
-                startIntent?.putExtra(key, value)
-            }
-            is Parcelable -> {
-                startIntent?.putExtra(key, value)
-            }
-        }
-        return this
-    }
-
-    fun navigation(context: Context) {
-        try {
-            val targetActivity = Class.forName(targetActivityClass)
-            if (null == startIntent)
-                startIntent = Intent()
-            startIntent?.setClass(context, targetActivity)
-            if (flags > 0)
-                startIntent?.setFlags(flags)
-            if (requestCode > -1) {
-                if (context is Activity) {
-                    context.startActivityForResult(startIntent, requestCode)
-                } else {
-                    Log.e(
-                        TAG,
-                        "$context can not call startActivityForResult() method with requestCode $requestCode"
-                    )
-                }
-            } else {
-                context.startActivity(startIntent)
-            }
-        } catch (e: Throwable) {
-            Log.e(TAG, "error while start Activity = $targetActivityClass, e = $e")
-        }
+        return route
     }
 
     fun inject(target: Any) {
         val targetActivityAutowired =
-            Class.forName("${target::class.java.canonicalName}_AutoWired")
+            Class.forName("${target::class.java.canonicalName}_Autowired")
         val injectMethod = targetActivityAutowired.getMethod("inject", Any::class.java)
         injectMethod.invoke(null, target)
     }
